cec42c9dc3ac25a5478f10df6abb66b80a0d5d72
Gary N <gary.newton@ymail.com>
2022-12-01 13:12:13 -0500

    Take one Down and Pass it Around application updated.

--- server.js
+++ server.js
@@ -7,19 +7,28 @@ app.get("/", function (req, res) {
               <h2><a href="/98">Take one down, pass it around.</a></h2>`);
 });
 
-app.get("/:number_of_bottles", function (req, res) {
+// app.get("/:number_of_bottles", function (req, res) {
+//     if(req.params.number_of_bottles > 0) {
+//         res.send(`<h1>${req.params.number_of_bottles} bottles of beer on the wall, ${req.params.number_of_bottles} bottles of beer.
+//                   </br>
+//                   <a href="./${req.params.number_of_bottles-1}">Take one down, pass it around.</a></h1>`)
+//     }
+//     else if (req.params.number_of_bottles <= 0) { 
+//         res.send("<h1> No more bottles of beer on the wall. <a href="/"> Start over. </a></h1>")
+//     }
+//     else{
+//         res.send("<h1> Something went wrong....</h1>")
+//     }
+// });
+
+app.get('/:number_of_bottles', (req, res) => {
     if(req.params.number_of_bottles > 0) {
-        res.send(`<h1>${req.params.number_of_bottles} bottles of beer on the wall, ${req.params.number_of_bottles} bottles of beer.
-                  </br>
-                  <a href="./${req.params.number_of_bottles-1}">Take one down, pass it around.</a></h1>`)
+     res.send(`<h1>${req.params.number_of_bottles} bottles of beer on the wall, ${req.params.number_of_bottles} bottles of beer.</br>
+     <a href="./${req.params.number_of_bottles - 1}">Take one down, pass it around.</a></h1>`);
+    } else {
+     res.send(`<h1> No more beer on the wall. <a href="/"> Start over. </a></h1>`);
     }
-    else if (req.params.number_of_bottles <= 0) { 
-        res.send("<h1> No more bottles of beer on the wall. <a href="/"> Start over. </a></h1>")
-    }
-    else{
-        res.send("<h1> Something went wrong....</h1>")
-    }
-});
+ });
 
 app.listen(PORT, function () {
     console.log("Listening on port: " + PORT);
